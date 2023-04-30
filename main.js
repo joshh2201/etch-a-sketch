36f91cdbe8fd171558938256484cafa22bcb0692
Josh <joshh2201@gmail.com>
2023-04-29 23:13:21 -0400

    Change addHover to randomize color if btn clicked

--- main.js
+++ main.js
@@ -8,20 +8,39 @@ function initializeGrid() {
       col.classList.add('col');
       row.appendChild(col);
     }
-
     container.appendChild(row);
   }
   addHover();
 }
 
-function changeColour() {
-  this.style.background = 'black';
+function changeColour(e) {
+  let rgbStr = '';
+  console.log(e.currentTarget.rainbow);
+  if (e.currentTarget.rainbow) {
+    rgbStr = `rgb(${randomRGB()},${randomRGB()},${randomRGB()})`;
+  } else {
+    rgbStr = 'rgb(0,0,0)';
+  }
+  this.style.background = rgbStr;
 }
 
-function addHover() {
+function addHover(isRainbow = false) {
   const squares = document.querySelectorAll('.col');
-  squares.forEach((square) =>
-    square.addEventListener('mouseover', changeColour)
-  );
+  squares.forEach((square) => {
+    square.removeEventListener('mouseover', changeColour);
+    square.rainbow = isRainbow;
+    square.addEventListener('mouseover', changeColour);
+  });
 }
+
+function randomRGB() {
+  return Math.floor(Math.random() * 256);
+}
+
+function multiColor() {
+  addHover(true);
+}
+
 initializeGrid();
+const rainbowBtn = document.querySelector('.rainbow');
+rainbowBtn.addEventListener('click', multiColor);
